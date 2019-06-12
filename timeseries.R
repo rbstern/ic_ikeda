760d118c4d63bb426505cb0ac2a570108f7ede52
rbstern <rbstern@gmail.com>
2019-06-12 10:42:08 -0300

    teste do stan com series temporais.

--- timeseries.R
+++ timeseries.R
@@ -1,16 +1,50 @@
 library(rstan)
 
-n = 12
-alpha = 7
-beta = 3
+###########################
+# Dados criados da cabeça #
+###########################
 
+N = 12
 #o que esses dados representam?
-myvector <- c(6,8,7,5,98,56,24,74,2,84,51,35)
-myts <- ts(myvector, start=c(2009, 1), end=c(2014, 12), frequency=12) 
+y <- c(6, 8, 7,5, 98, 56, 24, 74, 2, 84, 51, 35)
 
 amostra = stan(file = "timeseries.stan", 
-               data = c("n", "myts"),
+               data = c("N", "y"),
                iter = 10^5, chains = 1)
+
+# Estimacao
+alpha_amostra = extract(amostra, "alpha")[["alpha"]]
+beta_amostra = extract(amostra, "beta")[["beta"]]
+mean(alpha_amostra)
+mean(beta_amostra)
+
+# Intervalos de credibilidade
+plot(amostra, ci_level = 0.8, outer_level = 0.95)
+
+###################
+# Dados simulados #
+###################
+
+# Parametros
+alpha = 50
+beta = 0.3
+sigma = 0.1
+
+# Geração dos dados
+N = 10^3
+y = rep(NA, N)
+y[1] = sum(alpha*beta^c(0:100))
+for(i in 2:N)
+{
+  y[i] <- rnorm(1, alpha + beta*y[i-1], sigma)
+}  
+# Visualização dos dados
+plot(as.ts(y))
+
+# Uso do stan
+amostra = stan(file = "timeseries.stan", 
+               data = c("N", "y"),
+               iter = 10^4, chains = 1)
 
 # Estimacao
 alpha_amostra = extract(amostra, "alpha")[["alpha"]]
--- timeseries.stan
+++ timeseries.stan
@@ -1,13 +1,17 @@
 data {
-int<lower=0> N;
-vector[N] y;
+ int<lower=0> N;
+ vector[N] y;
 }
+
 parameters {
-real alpha;
-real beta;
-real<lower=0> sigma;
+ real alpha;
+ real <lower = -1, upper = 1> beta;
+ real <lower=0> sigma;
 }
+
 model {
-for (n in 2:N)
-y[n] ~ normal(alpha + beta * y[n-1], sigma);
+ alpha ~ normal(0, 1);
+ beta ~ normal(0, 0.1);
+ for (n in 2:N)
+  y[n] ~ normal(alpha + beta * y[n-1], sigma);
 }
